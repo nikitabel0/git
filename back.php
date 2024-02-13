1d416204d713045622fccc2c2ad664505b2dc2ba
nikitabel0 <ndbeliaev04012005@mail.ru>
2024-02-13 15:09:05 +0300

    add lab2

---
+++ back.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <?php 
+        print_r(get_headers('https://slavaver.github.io/web-course-site/labs/web-client-dev/2023/laba02/'))
+        ?>
+        <a href="lab2.php">переход обратно</a>
+    </main>
+</body>
+</html>
