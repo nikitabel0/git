708836180c3d198b1440875e21f313916a79f57f
nikitabel0 <ndbeliaev04012005@mail.ru>
2024-02-13 12:31:58 +0300

    add

---
+++ lab1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <img width="300px" src="/1J40dKg0YYY.jpg" alt="">
+            <h1 class="text">Hello, World!</h1>
+        </div>
+    </header>
+    <main>
+    <?php
+        for($i=1; $i<=10;$i++):
+    ?>
+    <p>лаба 1</p>  
+    <?php endfor; ?>
+
+    </main>
+
+   <footer>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, tempore illo libero omnis accusantium saepe dolor voluptate fugiat eaque, voluptatem impedit ipsa similique possimus quidem illum vitae aliquid doloribus eligendi? Mollitia veniam unde laboriosam, dignissimos labore numquam nam delectus distinctio sed ratione dolorem quisquam animi modi eum ad blanditiis libero similique nulla? Odio animi dolore molestiae voluptatibus earum quasi optio.</p> 
+   </footer>
+
+    
+
+</body>
+</html>
